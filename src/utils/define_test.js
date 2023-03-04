5db0398b09a3bbbb8d61f3a0ff13f691d7a6c463
0xdeenz <sergio@deenz.dev>
2023-03-04 23:14:05 +0100

    fixes

--- src/utils/define_test.js
+++ src/utils/define_test.js
@@ -1,11 +1,12 @@
 const keccak256 = require('keccak256')
-const { rootFromLeafArray } = require("../../src/poseidon")
+const { poseidon, rootFromLeafArray } = require("../../src/poseidon")
 
 const correctMultipleChoiceAnswers = new Array(64).fill('1') 
 const correctOpenAnswers = new Array(64).fill('deenz') 
 
 console.log("multipleChoiceRoot: ", rootFromLeafArray(getMultipleChoiceAnswersArray(correctMultipleChoiceAnswers)).toString())
 console.log("answerHashesRoot: ", rootFromLeafArray(getOpenAnswersArray(correctOpenAnswers)).toString())
+console.log("answerHashes: ", getOpenAnswersArray(correctOpenAnswers))
 
 function getMultipleChoiceAnswersArray( multipleChoiceAnswers ) {
     const answersArray = new Array(64).fill('0')
@@ -26,7 +27,7 @@ function getOpenAnswersArray( openAnswers ) {
         BigInt('0x' + keccak256("").toString('hex'))
     )
     resultsArray.forEach( (_, i) => { if (i < openAnswers.length) {
-        resultsArray[i] = BigInt('0x' + keccak256(openAnswers[i]).toString('hex'))
+        resultsArray[i] = poseidon([BigInt('0x' + keccak256(openAnswers[i]).toString('hex'))])
     }})
     return resultsArray
 }
